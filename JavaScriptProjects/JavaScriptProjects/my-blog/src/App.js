4d4f99e09e6fde90c8e3f5d5fb67c35c3f044267
vidyasagar2110 <tvidyasagar2110@gmail.com>
2025-01-04 08:56:43 +0530

    git

--- JavaScriptProjects/JavaScriptProjects/my-blog/src/App.js
+++ JavaScriptProjects/JavaScriptProjects/my-blog/src/App.js
@@ -1,20 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar.js';
+import Home from './Home.js';
 function App() {
-  const myStr="Hello";
-  const a=2.5;
-  const myLink="https://github.com/vidyasagar2110";
-  return (
-  <div className="App">
-  <div className="content">
-  <h1>Hello World</h1>
-  <h1>{myStr}</h1>
-  <h1>{a}</h1>
-  <a href={myLink}>My GitHub</a>
-  </div>
-  </div>
-  );
- }
-
+ return (
+ <div className="App">
+ <Navbar/>
+ <div className="content">Welcome to My App</div>
+ <Home />
+ </div>
+ );
+}
 export default App;
---
+++ JavaScriptProjects/JavaScriptProjects/my-blog/src/Home.js
@@ -0,0 +1,8 @@
+const Home = () => {
+    return ( 
+    <div className="home">
+    <h2> Home Page</h2>
+    </div>
+    );
+   }
+   export default Home;
