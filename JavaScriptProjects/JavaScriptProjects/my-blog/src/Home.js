10b3a0c96928842d20a58356a45187b6143b9a38
vidyasagar2110 <tvidyasagar2110@gmail.com>
2025-01-04 09:37:41 +0530

    git

--- JavaScriptProjects/JavaScriptProjects/my-blog/src/Home.js
+++ JavaScriptProjects/JavaScriptProjects/my-blog/src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    const handleClick = (e) => {
+    console.log("Hello ",e);
+    }
+    const handleClickAgain = (name,e) => {
+    console.log("Hello "+name,e.target);
+    }
     return ( 
     <div className="home">
     <h2> Home Page</h2>
+    <button onClick={handleClick}>Click me</button>
+    <button onClick={(e) => handleClickAgain('Ramu',e)}>Click me Again</button>
     </div>
     );
    }
