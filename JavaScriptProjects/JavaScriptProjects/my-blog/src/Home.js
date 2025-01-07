ec61823e336aecb5e9f81f36c7308e468e22d881
vidyasagar2110 <tvidyasagar2110@gmail.com>
2025-01-07 10:40:18 +0530

    git

--- JavaScriptProjects/JavaScriptProjects/my-blog/src/Home.js
+++ JavaScriptProjects/JavaScriptProjects/my-blog/src/Home.js
@@ -1,18 +1,19 @@
 import { useState } from "react";
 const Home = () => {
- const [name,setName]=useState('Sagar');
- const [age,setAge]=useState('20');
- const handleClick = () => {
- console.log("Hello Everyone");
- setAge('35');
- setName("Raju");
- }
+ const [blogs,setBlogs]=useState([
+ {title:"My Blog1", body:"Body of Blog1", author:"Sagar", id:1},
+ {title:"My Blog2", body:"Body of Blog2", author:"Rahul", id:2},
+ {title:"My Blog3", body:"Body of Blog3", author:"Ashish", id:3}
+ ]);
  return ( 
  <div className="home">
- <h2> Home Page</h2>
- <p> Name: {name}, Age: {age} </p>
- <button onClick={handleClick}>Click 
-me</button>
+ {blogs.map((blog) => (
+ <div className="blog-preview" key={blog.id}>
+ <h2>{blog.title}</h2>
+ <p>Written by: {blog.author}</p>
+ </div>
+ ))
+ }
  </div>
  );
 }
