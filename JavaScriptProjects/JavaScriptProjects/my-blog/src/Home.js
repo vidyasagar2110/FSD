b95576459de08170d81c071b3cd58fca808aadf2
vidyasagar2110 <tvidyasagar2110@gmail.com>
2025-01-07 11:20:13 +0530

    git

--- JavaScriptProjects/JavaScriptProjects/my-blog/src/Home.js
+++ JavaScriptProjects/JavaScriptProjects/my-blog/src/Home.js
@@ -2,13 +2,15 @@ import { useState } from "react";
 import BlogList from './BlogList';
 const Home = () => {
  const [blogs,setBlogs]=useState([
-    {title:"My Blog1", body:"Body of Blog1", author:"Sagar", id:1},
-    {title:"My Blog2", body:"Body of Blog2", author:"Rahul", id:2},
-    {title:"My Blog3", body:"Body of Blog3", author:"Ashish", id:3}
+ {title:"My Blog1", body:"Body of Blog1", author:"Sagar", id:1},
+ {title:"My Blog2", body:"Body of Blog2", author:"Rahul", id:2},
+ {title:"My Blog3", body:"Body of Blog3", author:"Ashish", id:3},
+ 
  ]);
  return ( 
  <div className="home">
  < BlogList blogs={blogs} title="All Blogs!" />
+ < BlogList blogs={blogs.filter((blog) => blog.author === 'Sagar')} title="Sagar's blogs" />
  </div>
  );
 }
