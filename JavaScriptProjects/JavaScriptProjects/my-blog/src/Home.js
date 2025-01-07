c51bc3ec7a216d5f5a5f32389fed055d6452b240
vidyasagar2110 <tvidyasagar2110@gmail.com>
2025-01-07 12:27:13 +0530

    git

--- JavaScriptProjects/JavaScriptProjects/my-blog/src/Home.js
+++ JavaScriptProjects/JavaScriptProjects/my-blog/src/Home.js
@@ -3,20 +3,29 @@ import BlogList from './BlogList';
 const Home = () => {
  const [blogs,setBlogs]=useState(null);
  const [isPending, setIsPending]=useState(true);
+ const [error,setError]=useState(null);
  useEffect(() => {
  setTimeout( () => {
- fetch('http://localhost:3000/blogs')
+ fetch('http://localhost:8000/blogs')
  .then(res => {
+ if (!res.ok){
+ throw Error("Could not fetch data from the resource");
+ }
  return res.json();
  })
  .then(data => {
  setBlogs(data);
  setIsPending(false);
  })
+ .catch(err => {
+ setIsPending(false)
+ setError(err.message)
+ })
  }, 5000);
  },[]);
- return ( 
- <div className="home">
+ return (
+    <div className="home">
+ { error && <div> {error} </div>}
  { isPending && <div>Loading... </div> }
  { blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
  </div>
