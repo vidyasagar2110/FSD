8b93727af2b589c4eb67384c68ae8a16616ad782
vidyasagar2110 <tvidyasagar2110@gmail.com>
2025-01-07 11:28:21 +0530

    git

--- JavaScriptProjects/JavaScriptProjects/my-blog/src/Home.js
+++ JavaScriptProjects/JavaScriptProjects/my-blog/src/Home.js
@@ -7,10 +7,15 @@ const Home = () => {
  {title:"My Blog3", body:"Body of Blog3", author:"Ashish", id:3},
  
  ]);
+ const handleDelete = (id) => {
+ const newBlogs=blogs.filter(blog =>
+blog.id !== id);
+ setBlogs(newBlogs);
+ }
  return ( 
  <div className="home">
- < BlogList blogs={blogs} title="All Blogs!" />
- < BlogList blogs={blogs.filter((blog) => blog.author === 'Sagar')} title="Sagar's blogs" />
+ < BlogList blogs={blogs} title="All 
+Blogs!" handleDelete={handleDelete} />
  </div>
  );
 }
